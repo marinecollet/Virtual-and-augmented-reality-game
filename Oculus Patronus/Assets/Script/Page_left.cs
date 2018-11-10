4a34978d9537b6ecb04c781937e27ee8c0c513de
marinecollet <marine.collet10@gmail.com>
2018-11-10 18:42:44 +0100

    Changement de sort sur le grimoire
    Ajout du materiau de la page gauche avec le premier sort
    Changement de materiau, donc de sort, lorsqu'on touche la page gauche du grimoire avec la baguette

---
+++ Oculus Patronus/Assets/Script/Page_left.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Page_left : MonoBehaviour {
+
+    private Renderer renderer;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.CompareTag("Wand"))
+        {
+            renderer = GetComponent<SkinnedMeshRenderer>();
+            renderer.material = Resources.Load("material1", typeof(Material)) as Material;
+        }
+    }
+    
+
+
+}
