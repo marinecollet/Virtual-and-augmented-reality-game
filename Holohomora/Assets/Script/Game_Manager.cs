972e808483af9f913c1eaee00018a8c02da5a0cb
marinecollet <marine.collet10@gmail.com>
2018-11-11 20:13:46 +0100

    Hoculus project
    Creation of the Holohomora project
    Creation of a maze
    Adding doors

---
+++ Holohomora/Assets/Script/Game_Manager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game_Manager : MonoBehaviour {
+
+    public Maze mazePrefab;
+
+    private Maze mazeInstance;
+
+	void Start ()
+    {
+        BeginGame();
+	}
+	
+	void Update ()
+    {
+		if (Input.GetKeyDown(KeyCode.Space))
+        {
+            RestartGame();
+        }
+	}
+
+    void BeginGame()
+    {
+        mazeInstance = Instantiate(mazePrefab) as Maze;
+        StartCoroutine(mazeInstance.Generate());
+    }
+
+    void RestartGame()
+    {
+        StopAllCoroutines();
+        Destroy(mazeInstance.gameObject);
+        BeginGame();
+    }
+}
