a03754ccfb8797196ebf3d44c7a2ff38ab174739
marinecollet <marine.collet10@gmail.com>
2018-11-20 22:01:49 +0100

    Added the maze

---
+++ Oculus Patronus/Assets/Script/Maze/Game_Manager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game_Manager : MonoBehaviour {
+
+    public Maze mazePrefab;
+
+    private Maze mazeInstance;
+
+	void Start ()
+    {
+        BeginGame();
+	}
+	
+	void Update ()
+    {
+		if (Input.GetKeyDown(KeyCode.Space))
+        {
+            RestartGame();
+        }
+	}
+
+    void BeginGame()
+    {
+        mazeInstance = Instantiate(mazePrefab) as Maze;
+        mazeInstance.Generate();
+    }
+
+    void RestartGame()
+    {
+        Destroy(mazeInstance.gameObject);
+        BeginGame();
+    }
+}
