5f20c087880c1d211aa884c2406bd1abcaf01ecd
marinecollet <marine.collet10@gmail.com>
2018-12-26 13:53:34 +0100

    Changing the grimoire's material with the wand

--- Oculus Patronus/Assets/Script/Page_left.cs
+++ Oculus Patronus/Assets/Script/Page_left.cs
@@ -2,14 +2,54 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Page_left : MonoBehaviour {
-       
+public class Page_left : MonoBehaviour
+{
+    public Renderer page_right;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Wand"))
         {
             Renderer renderer = GetComponent<SkinnedMeshRenderer>();
-            renderer.material = Resources.Load("material_page_1", typeof(Material)) as Material;
+            string name_renderer = renderer.material.name;
+            Debug.Log(name_renderer);
+
+            changeMaterial(name_renderer);
+        }
+    }
+
+    void changeMaterial(string name)
+    {
+        Renderer page_left = GetComponent<SkinnedMeshRenderer>();
+        switch (name)
+        {
+            case "page_left (Instance)":
+                break;
+
+            case "material_page_1 (Instance)":
+                page_left.material = Resources.Load("page_left", typeof(Material)) as Material;
+                page_right.material = Resources.Load("page_right", typeof(Material)) as Material;
+                break;
+
+            case "material_page_2 (Instance)":
+                page_left.material = Resources.Load("material_page_1", typeof(Material)) as Material;
+                page_right.material = Resources.Load("material_page_1", typeof(Material)) as Material;
+                break;
+
+            case "material_page_3 (Instance)":
+                page_left.material = Resources.Load("material_page_2", typeof(Material)) as Material;
+                page_right.material = Resources.Load("material_page_2", typeof(Material)) as Material;
+                break;
+
+            case "material_page_4 (Instance)":
+                page_left.material = Resources.Load("material_page_3", typeof(Material)) as Material;
+                page_right.material = Resources.Load("material_page_3", typeof(Material)) as Material;
+                break;
+
+            default:
+                page_left.material = Resources.Load("page_left", typeof(Material)) as Material;
+                page_right.material = Resources.Load("page_right", typeof(Material)) as Material;
+                break;
         }
     }
 }
---
+++ Oculus Patronus/Assets/Script/Page_right.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Page_right : MonoBehaviour {
+
+    public Renderer page_left;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Wand"))
+        {
+            Renderer renderer = GetComponent<SkinnedMeshRenderer>();
+            string name_renderer = renderer.material.name;
+            Debug.Log(name_renderer);
+
+            changeMaterial(name_renderer);
+        }
+    }
+
+    void changeMaterial(string name)
+    {
+        Renderer page_right = GetComponent<SkinnedMeshRenderer>();
+        switch (name)
+        {
+            case "page_right (Instance)":
+                page_left.material = Resources.Load("material_page_1", typeof(Material)) as Material;
+                page_right.material = Resources.Load("material_page_1", typeof(Material)) as Material;
+                break;
+
+            case "material_page_1 (Instance)":
+                page_left.material = Resources.Load("material_page_2", typeof(Material)) as Material;
+                page_right.material = Resources.Load("material_page_2", typeof(Material)) as Material;
+                break;
+
+            case "material_page_2 (Instance)":
+                page_left.material = Resources.Load("material_page_3", typeof(Material)) as Material;
+                page_right.material = Resources.Load("material_page_3", typeof(Material)) as Material;
+                break;
+
+            case "material_page_3 (Instance)":
+                page_left.material = Resources.Load("material_page_4", typeof(Material)) as Material;
+                page_right.material = Resources.Load("material_page_4", typeof(Material)) as Material;
+                break;
+
+            case "material_page_4 (Instance)":
+                break;
+
+            default:
+                break;
+        }
+    }
+}
