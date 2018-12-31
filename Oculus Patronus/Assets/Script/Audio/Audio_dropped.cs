f1cea434d88a937b2693679ac7605748218ea953
marinecollet <marine.collet10@gmail.com>
2018-12-31 11:59:15 +0100

    Sounds when movable objects are dropped

---
+++ Oculus Patronus/Assets/Script/Audio/Audio_dropped.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Audio_dropped : MonoBehaviour {
+
+
+    public AudioSource audioSource;
+
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if ((collision.relativeVelocity.magnitude > 2) && (gameObject.transform.parent == null))
+        {
+            audioSource.Play();
+        }
+        else if((collision.relativeVelocity.magnitude > 2) && (gameObject.transform.parent.name != "pivot"))
+        {
+            audioSource.Play();
+        }
+    }
+
+
+}
