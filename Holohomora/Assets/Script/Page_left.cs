51ab3d0be3d9646e00a46a5395974ad79e848af4
marinecollet <marine.collet10@gmail.com>
2019-01-01 13:12:27 +0100

    Grimoire material on touch - Holohomora

--- Holohomora/Assets/Script/Page_left.cs
+++ Holohomora/Assets/Script/Page_left.cs
@@ -2,29 +2,54 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Page_left : MonoBehaviour {
-
-    private Renderer renderer;
-
+public class Page_left : MonoBehaviour
+{
+    public Renderer page_right;
 
     private void OnTriggerEnter(Collider other)
     {
-
         if (other.gameObject.CompareTag("Wand"))
         {
-            renderer = GetComponent<SkinnedMeshRenderer>();
-            renderer.material = Resources.Load("material1", typeof(Material)) as Material;
+            Renderer renderer = GetComponent<SkinnedMeshRenderer>();
+            string name_renderer = renderer.material.name;
+            Debug.Log(name_renderer);
+
+            changeMaterial(name_renderer);
         }
     }
 
-    public void Update()
+    void changeMaterial(string name)
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        Renderer page_left = GetComponent<SkinnedMeshRenderer>();
+        switch (name)
         {
-            renderer = GetComponent<SkinnedMeshRenderer>();
-            renderer.material = Resources.Load("material1", typeof(Material)) as Material;
+            case "page_left (Instance)":
+                break;
+
+            case "material_page_1 (Instance)":
+                page_left.material = Resources.Load("page_left", typeof(Material)) as Material;
+                page_right.material = Resources.Load("page_right", typeof(Material)) as Material;
+                break;
+
+            case "material_page_2 (Instance)":
+                page_left.material = Resources.Load("material_page_1", typeof(Material)) as Material;
+                page_right.material = Resources.Load("material_page_1", typeof(Material)) as Material;
+                break;
+
+            case "material_page_3 (Instance)":
+                page_left.material = Resources.Load("material_page_2", typeof(Material)) as Material;
+                page_right.material = Resources.Load("material_page_2", typeof(Material)) as Material;
+                break;
+
+            case "material_page_4 (Instance)":
+                page_left.material = Resources.Load("material_page_3", typeof(Material)) as Material;
+                page_right.material = Resources.Load("material_page_3", typeof(Material)) as Material;
+                break;
+
+            default:
+                page_left.material = Resources.Load("page_left", typeof(Material)) as Material;
+                page_right.material = Resources.Load("page_right", typeof(Material)) as Material;
+                break;
         }
     }
-
-
 }
